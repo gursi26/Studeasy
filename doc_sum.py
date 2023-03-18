14007c95ab64023b33d8643de6d6718c86d096fa
gursi26 <gursimar.batra@gmail.com>
2023-03-18 10:56:35 -0400

    Added template functions

---
+++ doc_sum.py
@@ -0,0 +1,8 @@
+def research_paper_summary(text_body: str, prompt: str, max_num_words: int) -> str:
+    pass
+
+def textbook_summary(text_body: str, prompt: str, max_num_words: int) -> str:
+    pass
+
+def book_summary(text_body: str, prompt: str, max_num_words: int) -> str:
+    pass
---
+++ test_companion.py
@@ -0,0 +1,5 @@
+def test_companion(test_str: str) -> dict[str, str]:
+    pass
+
+def grade_answer(question: str, answer: str, correct_answer: str) -> tuple[float, str]:
+    pass
